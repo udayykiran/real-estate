d0bc7ebf11e96e5f1fb970bff0be06c6dbcec6aa
uday kiran <ukreddymanda@gmail.com>
2024-05-10 17:35:26 +0530

    user model email added

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,16 +1,24 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    username:{
-        type: String,
-        required: true,
-        unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type: String,
-        required: true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-}, { timestamps: true });
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model("User", userSchema);
 export default User;
